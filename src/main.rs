abe54a48af17e2ac5b161232230c382a438291b0
apambrose <austin.p.ambrose@gmail.com>
2023-04-30 00:03:41 -0700

    Cleaned things up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 use clap::Parser;
-use std::path::PathBuf;
-use std::fs;
 
-/// Simple program to greet a person
+/// Control your home lighting from the terminal
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
 
     /// Set the brightness from 0% to 100%
     brightness: Option<u8>,
-
-    /// Register a new computer for the first time
-    #[arg(short, long, default_value_t = false)]
-    register: bool,
 
     /// Only affect the bedroom lights
     #[arg(short, long, default_value_t = false)]
@@ -29,16 +23,21 @@ struct Args {
     /// Only affect the living room lights
     #[arg(short, long, default_value_t = false)]
     livingroom: bool,
+
+    // Debug
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
 }
 
 fn main() {
     
+    // Handle all CLI parsing
     let args = Args::parse();
-    // Reformat brightness from 0 to 100
-    let user = get_computer_name();
+
+    // Create an object for the Hue Bridge
     let key  = "Xg-MbGdlRZlXEUqeaEQSNVai2n91W4qPSmrBWrLR";
     let bridge = match hueclient::Bridge::discover() {
-        Some(bridge) => bridge,
+        Some(bridge) => bridge.with_user(key),
         None         => {
             println!{"Failed to find a Hue Bridge"}
             println!{"Make sure you're connected to the network and the bridge is powered on..."}; 
@@ -46,112 +45,33 @@ fn main() {
         }
     };
     
-    // Register a new user and exit if the user passes the --register flag
-    if args.register == true {
-        match bridge.register_user(&user) {
-            Ok(bridge)  => {
-                println!("Successfully registered user: {}, with Hue Bridge :)", &user);
-                println!("New key is: {}", &bridge.username);
-                save_config(&bridge.username);
-            },
-            Err(_) => {
-                println!("Failed to register with the Hue Bridge...");
-                println!("Make sure you press the button on the Bridge before");
-                println!("running this command...");
-                return;
-            }
-        };
-        
-        return;
+
+    if args.debug {
+        for light in &bridge.get_all_lights().unwrap() {
+            let light = &light.light;
+            println!("name: {}, state: {:?}", light.name, light.state);
+        }
     }
-
-    // Try to load the username from the config file
-    // let username = match load_config() {
-    //     Ok(username) => username,
-    //     Err(msg)     => {
-    //         println!("{msg}");
-    //         return;
-    //     }
-    // };
-
-    
-    // Connect to the bridge with the username
-    let bridge = bridge.with_user(key);
-    
-
-    // for light in &bridge.get_all_lights().unwrap() {
-    //     println!("{:?}", light);
-    // }
 
     // If the user wants to change the brightness, do it
     if let Some(mut brightness) = args.brightness {
-          brightness = (brightness as f32 * 2.54) as u8;
-        let cmd = hueclient::CommandLight::default().with_bri(brightness);
+
+        // Tell the light to turn off if brightness == 0
+        // Otherwise, tell it to turn on to brightness
+        let mut cmd = hueclient::CommandLight::default();
+        if brightness == 0 {
+            cmd.on = Some(false);
+        } else {
+            brightness = (brightness as f32 * 2.54) as u8;
+            cmd.on = Some(true);
+            cmd.bri = Some(brightness);
+        }
+
+        // Update the lights
         for light in &bridge.get_all_lights().unwrap() {
-            if light.light.name.contains("Bedroom 1") {
+            if light.light.name.contains("Bedroom") {
                 bridge.set_light_state(light.id, &cmd).unwrap();
             }
         }
     }
 }
-
-
-fn get_computer_name() -> String {
-    let os_name = hostname::get()
-                    .expect("Failed to get name of computer");
-    let str_name = os_name.to_str()
-                    .expect("Failed to convert computer name to string");
-    return String::from(str_name);
-}
-
-fn save_config(username : &str) {
-
-    // Get the path to the config file
-    let config_path = get_config_path();
-
-    // Write to it
-    let write_result = fs::write(&config_path, username);
-
-    // Report any errors
-    if let Err(_) = write_result {
-        println!("ERROR: Failed to write config file > {}", config_path.to_str().unwrap() )    
-    }
-}
-
-fn load_config() -> Result<String, String> {
-
-    // Get the path to the config file
-    let config_path = get_config_path();
-
-    // Throw a nice error if it doesn't exist
-    if config_path.exists() == false {
-        return Err(format!("ERROR: config file {} does not exist.
-                           \rTry Registering your computer with `lit --register`"
-                           ,config_path.to_str().unwrap()));
-    }
-
-    // Read it if it exists
-    let config_contents = fs::read(&config_path);
-
-    //Check for a read error
-    let config_username = match config_contents {
-        Ok(contents) => contents,
-        Err(_) => return Err(format!("ERROR: Error reading file {}", config_path.to_str().unwrap())),
-    };
-
-    // If there's nothing in it, return an error
-    if config_username.len() == 0 {
-        return Err(format!("ERROR: config file {} exists, but is empty", config_path.to_str().unwrap()));
-    } else {
-        return Ok(config_username[0].to_string());
-    }
-}
-
-fn get_config_path() -> PathBuf {
-    // Make sure a config file exists, and that we can pull a username from it
-    let current_exe = std::env::current_exe().unwrap();
-    let mut current_dir = current_exe.parent().unwrap().to_owned();
-    current_dir.push("lit.config");
-    current_dir
-}
-
